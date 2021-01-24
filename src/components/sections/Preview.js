f076b96ddfd3a6ee66e29df0ef57937ffac6efe1
Nathan Schneider <nathan.schneider@sap.com>
2021-01-24 17:36:14 -0300

    Add initial version of preview with hard-coded melody and export button

---
+++ src/components/sections/Preview.js
@@ -0,0 +1,67 @@
+import {
+  PianoRollSVGVisualizer,
+  sequenceProtoToMidi,
+  SoundFontPlayer,
+} from "@magenta/music";
+import { saveAs } from "file-saver";
+import { Button } from "antd";
+import { useEffect, useState } from "react";
+import "./Preview.css";
+import { PlayCircleOutlined, StopOutlined } from "@ant-design/icons";
+
+let visualizer, player;
+
+const someSequence = {
+  tempos: [{ qpm: 120 }],
+  notes: [
+    { pitch: 73, quantizedStartStep: "0", quantizedEndStep: "6" },
+    { pitch: 68, quantizedStartStep: "6", quantizedEndStep: "10" },
+    { pitch: 59, quantizedStartStep: "14", quantizedEndStep: "16" },
+    { pitch: 61, quantizedStartStep: "16", quantizedEndStep: "22" },
+    { pitch: 59, quantizedStartStep: "22", quantizedEndStep: "26" },
+  ],
+  quantizationInfo: { stepsPerQuarter: 4 },
+  totalQuantizedSteps: "32",
+};
+
+export default function Preview() {
+  function onPlayClick() {
+    player.start(someSequence);
+  }
+
+  function onExportClick() {
+    saveAs(new File([sequenceProtoToMidi(someSequence)], "sample.mid"));
+  }
+
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    visualizer = new PianoRollSVGVisualizer(
+      someSequence,
+      document.getElementById("previewSvg"),
+      {
+        noteRGB: "35,70,90",
+        activeNoteRGB: "157, 229, 184",
+        noteHeight: 5,
+      }
+    );
+    player = new SoundFontPlayer(
+      "https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus"
+    );
+  });
+  return (
+    <div style={{ position: "relative" }}>
+      <div className="visualizer-container cream">
+        <Button
+          className="button-circle"
+          icon={playing ? <StopOutlined /> : <PlayCircleOutlined />}
+          onClick={onPlayClick}
+        />
+        <svg id="previewSvg"></svg>
+      </div>
+      <Button type="primary" onClick={onExportClick}>
+        Export
+      </Button>
+    </div>
+  );
+}
