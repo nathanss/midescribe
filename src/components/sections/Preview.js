890b7b559907f0a7897b0e97045d295693a6e8ec
Nathan Schneider <nathan.schneider@sap.com>
2021-01-24 22:00:43 -0300

    Train midime model with sample sequence

--- src/components/sections/Preview.js
+++ src/components/sections/Preview.js
@@ -1,28 +1,102 @@
-import {
-  PianoRollSVGVisualizer,
-  sequenceProtoToMidi,
-  SoundFontPlayer,
-} from "@magenta/music";
 import { saveAs } from "file-saver";
 import { Button, Col, Row, Form } from "antd";
 import { useEffect, useState } from "react";
 import "./Preview.css";
 import { PlayCircleOutlined, StopOutlined } from "@ant-design/icons";
 import MidiMeSlider from "../musicData/MidiMeSlider";
+import { getChunks } from "./helpers";
+import {
+  MidiMe,
+  MusicVAE,
+  PianoRollSVGVisualizer,
+  sequenceProtoToMidi,
+  sequences,
+  SoundFontPlayer,
+} from "@magenta/music/es6";
 
 let visualizer, player;
 
 const someSequence = {
-  tempos: [{ qpm: 120 }],
+  //loaded from midi file
+  ticksPerQuarter: 480,
+  timeSignatures: [{ time: 0, numerator: 4, denominator: 4 }],
+  tempos: [{ time: 0, qpm: 120 }],
   notes: [
-    { pitch: 73, quantizedStartStep: "0", quantizedEndStep: "6" },
-    { pitch: 68, quantizedStartStep: "6", quantizedEndStep: "10" },
-    { pitch: 59, quantizedStartStep: "14", quantizedEndStep: "16" },
-    { pitch: 61, quantizedStartStep: "16", quantizedEndStep: "22" },
-    { pitch: 59, quantizedStartStep: "22", quantizedEndStep: "26" },
+    {
+      pitch: 72,
+      velocity: 102,
+      startTime: 0,
+      endTime: 0.125,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 74,
+      velocity: 102,
+      startTime: 0.125,
+      endTime: 0.25,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 76,
+      velocity: 102,
+      startTime: 0.25,
+      endTime: 0.375,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 77,
+      velocity: 102,
+      startTime: 0.375,
+      endTime: 0.5,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 79,
+      velocity: 102,
+      startTime: 0.5,
+      endTime: 0.625,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 81,
+      velocity: 102,
+      startTime: 0.625,
+      endTime: 0.75,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 83,
+      velocity: 102,
+      startTime: 0.75,
+      endTime: 0.875,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
+    {
+      pitch: 84,
+      velocity: 102,
+      startTime: 0.875,
+      endTime: 1,
+      instrument: 0,
+      // program: -1,
+      isDrum: false,
+    },
   ],
-  quantizationInfo: { stepsPerQuarter: 4 },
-  totalQuantizedSteps: "32",
+  totalTime: 1,
+  sourceInfo: { encodingType: "MIDI", parser: "TONEJS_MIDI_CONVERT" },
 };
 
 export default function Preview() {
@@ -32,6 +106,24 @@ export default function Preview() {
 
   function onExportClick() {
     saveAs(new File([sequenceProtoToMidi(someSequence)], "sample.mid"));
+  }
+
+  async function onTrainClick() {
+    const url =
+      "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small";
+    const mvae = new MusicVAE(url);
+
+    await mvae.initialize();
+    const midime = new MidiMe({
+      epochs: 100,
+    });
+    await midime.initialize();
+    const quantizedMel = sequences.quantizeNoteSequence(someSequence, 4);
+    const z = await mvae.encode(getChunks([quantizedMel]));
+    midime.train(z, async (epoch, logs) => {
+      console.log(epoch);
+      console.log(logs);
+    });
   }
 
   const [playing, setPlaying] = useState(false);
@@ -79,9 +171,8 @@ export default function Preview() {
         />
         <svg id="previewSvg"></svg>
       </div>
-      <Button type="primary" onClick={onExportClick}>
-        Export
-      </Button>
+      <Button onClick={onTrainClick}>Train</Button>
+      <Button onClick={onExportClick}>Export</Button>
     </div>
   );
 }
