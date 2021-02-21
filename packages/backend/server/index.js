5c2faa73747897cd54114db8d83f3a0af992cc4c
Nathan Schneider <nathan.schneider@sap.com>
2021-02-21 18:34:18 -0300

    Create analyzeEntities endpoint to forward to google's analyzeEntities

--- packages/backend/server/index.js
+++ packages/backend/server/index.js
@@ -14,27 +14,13 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.post("/getTextDescription", async (req, res) => {
+app.post("/analyzeEntities", async (req, res) => {
   const text = req.body.text;
-  // Instantiates a client
   const client = new language.LanguageServiceClient();
-
-  // The text to analyze
-
   const document = {
     content: text,
     type: 'PLAIN_TEXT',
   };
-
-  // Detects the sentiment of the text
-  client.ana
-  const [result] = await client.analyzeSentiment({document: document});
-  const sentiment = result.documentSentiment;
-
-  console.log(`Text: ${text}`);
-  console.log(`Sentiment score: ${sentiment.score}`);
-  console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  res.json({
-    message: "Done!"
-  })
+  const [result] = await client.analyzeEntities({document: document});
+  res.json(result);
 });
