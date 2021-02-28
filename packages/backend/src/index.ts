968a4acbe387b1c1e8101b004f4fa41b6d80ebfd
Nathan Schneider <nathan.schneider@sap.com>
2021-02-28 16:28:42 -0300

    Add typescript to back-end

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,43 @@
+import * as SongIdeaInterfaces from "../../frontend/src/musicGeneration/SongIdeaInterfaces";
+
+import express from "express";
+import bodyParser from "body-parser";
+import language from "@google-cloud/language";
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.post("/analyzeEntities", async (req, res) => {
+  const text = req.body.text;
+  const client = new language.LanguageServiceClient();
+  const [result] = await client.analyzeEntities({
+    document: {
+      content: text,
+      type: "PLAIN_TEXT",
+    },
+    encodingType: "UTF8",
+  });
+  res.json(result);
+});
+
+app.post("/analyzeSyntax", async (req: any, res: any) => {
+  const text = req.body.text;
+  const client = new language.LanguageServiceClient();
+  const [result] = await client.analyzeSyntax({
+    document: {
+      content: text,
+      type: "PLAIN_TEXT",
+    },
+    encodingType: "UTF8",
+  });
+  res.json(result);
+});
